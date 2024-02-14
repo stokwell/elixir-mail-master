f1f1e7999d62f9598f994ec2ca3109d5caa1d6d6
Vitaly Lyapunov <stokwell1@gmail.com>
2024-02-14 17:29:46 +0100

    Refactors application.ex (wip)

--- my_broadway_app/lib/my_broadway_app/application.ex
+++ my_broadway_app/lib/my_broadway_app/application.ex
@@ -1,8 +1,6 @@
 defmodule MyBroadwayApp.Application do
   use Application
 
-  import MyBroadwayApp.Events
-
   # MyBroadwayApp.Application.start(:normal, [])
 
   def start(_type, _args) do
@@ -13,41 +11,35 @@ defmodule MyBroadwayApp.Application do
     opts = [strategy: :one_for_one, name: MyBroadwayApp.Supervisor]
     Supervisor.start_link(children, opts)
 
-    create_initial_users()
-    publish_events()
+    get_or_generate_users() |> publish_events()
 
     {:ok, nil}
   end
 
 
-  defp create_initial_users do
-    IO.puts("Generating users...")
-
-    existing_users_count = length(MyBroadwayApp.UserStorage.get_all_users())
-    desired_user_count = 50
-    users_to_generate = desired_user_count - existing_users_count
-
-    if users_to_generate > 0 do
-      for _ <- 1..users_to_generate do
-        id = :crypto.strong_rand_bytes(8) |> Base.encode16()
-        first_name = Faker.Person.first_name()
-        last_name = Faker.Person.last_name()
-        email = "#{String.downcase(first_name)}_#{String.downcase(last_name)}@example.com"
-        balance = 0
-        MyBroadwayApp.UserStorage.add_user(id, email, balance)
-      end
-      IO.puts("Users generated successfully.")
+  defp get_or_generate_users do
+    if MyBroadwayApp.UserStorage.get_all_users() == [] do
+      IO.puts("Generating new users...")
+      generate_new_users()
     else
       IO.puts("Users already generated.")
     end
 
-    users =  MyBroadwayApp.UserStorage.get_all_users()
-    IO.inspect(users)
+    MyBroadwayApp.UserStorage.get_all_users()
   end
 
-  defp publish_events do
-    users = MyBroadwayApp.UserStorage.get_all_users()
+  def generate_new_users do
+    for _ <- 1..50 do
+      id = :crypto.strong_rand_bytes(8) |> Base.encode16()
+      first_name = Faker.Person.first_name()
+      last_name = Faker.Person.last_name()
+      email = "#{String.downcase(first_name)}_#{String.downcase(last_name)}@example.com"
+      balance = 0
+      MyBroadwayApp.UserStorage.add_user(id, email, balance)
+    end
+  end
 
+  defp publish_events(users) do
     # Run event handling tasks concurrently
     Enum.each(1..120, fn _ ->
       Task.async(fn ->
@@ -60,6 +52,8 @@ defmodule MyBroadwayApp.Application do
     |> Enum.map(&Task.await/1)
 
     IO.puts("Random events for 100 random users were published.")
+
+    {:ok, nil}
   end
 
 
