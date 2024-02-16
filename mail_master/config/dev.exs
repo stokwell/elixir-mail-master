7653d50b5bd86bd8a371c212484b667696bc5ee4
Vitaly Lyapunov <stokwell1@gmail.com>
2024-02-16 15:26:31 +0100

    Fixes database hostname

--- mail_master/config/dev.exs
+++ mail_master/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :mail_master, MailMaster.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "mail_master_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
